4398995c7868b996f9b900fa88f46c09aa22f3ec
Luca <lucat1@users.noreply.github.com>
2020-06-27 19:18:06 +0200

    feat: setup colors

--- jv.c
+++ jv.c
@@ -4,10 +4,12 @@
 #include <getopt.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 #include <json-c/json.h>
+#include "colors.h"
 
-const char *LINE = " %-14s ┃ %-5s ┃ %s\n";
+#define LINE GREEN_TEXT " %-14s " COLOR_RESET "│%s %-5s " COLOR_RESET "│ %s\n"
 
 int strict = 0;
 
@@ -34,15 +36,10 @@ int handle(char *line, size_t size) {
     default:
       if(strict) {
         // in strict mode we fail if the json is invalid
-        snprintf(
-          message,
-          sizeof message,
-          "invalid JSON at line: %i",
-          line_count
-          ); 
+        snprintf(message, sizeof message, "invalid JSON at line: %i", line_count);
         return 1;
       } else {
-        printf(LINE, "", "raw", line);
+        printf(LINE, "", MAGENTA_TEXT, "raw", line);
         return 0;
       }
   }
@@ -54,9 +51,27 @@ int handle(char *line, size_t size) {
   strftime(time_str, sizeof(time_str), "%D %R", localtime(&time));
 
   json_object *raw_level = get(root, "level");
-  const char *level = json_object_get_string(raw_level);
+  char *color = LIGHT_BLACK_TEXT; 
+  char *level = json_object_get_string(raw_level);
 
-  printf(LINE, time_str, level, "");
+  // convert the `level` to lowercase
+  for(int i = 0; level[i]; i++) {
+    level[i] = tolower(level[i]);
+  }
+
+  if(strcmp(level, "debug") == 0) {
+    color = LIGHT_BLUE_TEXT;
+  } else if (strcmp(level, "info") == 0) {
+    color = BLUE_TEXT;
+  } else if (strcmp(level, "success") == 0) {
+    color = GREEN_TEXT;
+  } else if (strcmp(level, "warn") == 0) {
+    color = YELLOW_TEXT;
+  } else if (strcmp(level, "error") == 0) {
+    color = RED_TEXT;
+  }
+
+  printf(LINE, time_str, color, level, "");
   return 0;
 }
 
